01cd756588c28350ebed832258cfceda8946d8ef
Leedoseo <okok0840@naver.com>
2025-06-02 16:38:04 +0900

    feature/schedule_card 시간을 표현할 Time위젯 제작

---
+++ lib/Widgets/schedule_card.dart
@@ -0,0 +1,39 @@
+import 'package:todolist_project/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class _Time extends StatelessWidget {
+  final int startTime;
+  final int endTime;
+
+  const _Time({
+    required this.startTime,
+    required this.endTime,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontWeight: FontWeight.w600,
+      color: PRIMARY_COLOR,
+      fontSize: 16.0,
+    );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "${startTime.toString().padLeft(2, "0")}:00",
+          style: textStyle,
+        ),
+        Text(
+          "${endTime.toString().padLeft(2, "0")}:00",
+          style: textStyle.copyWith(
+            fontSize: 10.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
