eee0ebdc6c4866d8948c3abc88cddcd126707d1e
Leedoseo <okok0840@naver.com>
2025-06-19 09:11:52 +0900

    일정 내용 텍스트 필드 위젯 구현

---
+++ lib/Widgets/custom_text_field.dart
@@ -0,0 +1,29 @@
+import 'package:todolist_project/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+
+  const CustomTextField({
+    requried this.laber,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Column( // 세로로 텍스트와 텍스트 필드 배치
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            color: PRIMARY_COLOR,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        TextFormField(), // 폼 안에서 텍스트 필드를 쓸 때 사용
+      ],
+    );
+  }
+}
