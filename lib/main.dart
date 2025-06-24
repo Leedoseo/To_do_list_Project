8924fa696167b4656952d139ae3c3e3523f48e90
Leedoseo <okok0840@naver.com>
2025-06-24 14:24:53 +0900

    달력 언어 변경(한국어)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:todolist_project/screen/calendar_screen.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // 플러터 프레임워크가 준비될 때까지 대기
+
+  await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
+
   runApp(
     MaterialApp(
       home: CalendarScreen(),
-    ),
+    )
   );
 }
